794bfd1f1b52cec676879b877ad01569a12b6b88
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-08-12 20:18:00 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  translate: TranslateService = inject(TranslateService);
   title = 'angular-18-app';
+  translateText(lang: string) {
+    this.translate.use(lang);
+  }
 }
